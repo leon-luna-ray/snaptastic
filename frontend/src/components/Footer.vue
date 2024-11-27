6f583b758f210429ebb355b31b89a8430b42bb2e
Ray Luna <leon.luna.ray@gmail.com>
2024-11-26 22:21:06 -0800

    update

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+<footer class="flex justify-center items-center py-[2rem]">
+    <div class="text-[0.75rem]">&copy; <a href="https://www.rayluna.dev" target="_blank">Ray Luna</a>  {{ new Data.getFullYear() }}</div>
+</footer>
+</template>
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Sanptastic</h1>
+    </div>
+</template>
