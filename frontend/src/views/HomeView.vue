6620a9e30c97b80dada9c920375865b7627b9cf1
Ray Luna <ray@hello-upswell.com>
2024-11-27 10:06:02 -0800

    fix frontend

--- frontend/src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Snaptastic</h1>
-    </div>
-</template>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url'
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   build: {
-    outDir: '../snaptastic/static/dist',
+    outDir: 'dist',
     rollupOptions: {
       external: ['src/main.js'],
       output: {
@@ -19,5 +13,9 @@ export default defineConfig({
         assetFileNames: 'index.css',
       },
     },
+  },
+  server: {
+    host: true,
+    port: 5173
   },
 });
