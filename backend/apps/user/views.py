85367c21b1c1fcb9773389bfbbb806a0915d060f
Ray Luna <leon.luna.ray@gmail.com>
2024-11-27 22:37:23 -0800

    setup user model, routes and endpoints

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserSerializer
+
+
+@api_view(["POST"])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    user = authenticate(request, email=email, password=password)
+
+    if user is not None:
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        return Response({"token": token.key, "user": serializer.data})
+    else:
+        return Response(
+            {"detail": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["POST"])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        user = serializer.instance
+        user.set_password(request.data["password"])
+        user.save()
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({"token": token.key, "user": serializer.data})
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response(f"passed for {request.user.email}")
