e160dd104daac34f360e34da8e55d4ec5e9a4221
Ray Luna <leon.luna.ray@gmail.com>
2024-12-02 21:57:00 -0800

    update signup

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -36,8 +36,9 @@ def login(request):
 @api_view(["POST"])
 def signup(request):
     serializer = UserSerializer(data=request.data)
-
+    print('🍒 in serializer')
     if serializer.is_valid():
+        print('🍒 in serializer is valid')
         serializer.save()
         user = serializer.instance
         user.set_password(request.data["password"])
