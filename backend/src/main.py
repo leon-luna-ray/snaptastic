f467658da72adc596249895782de2ff74f145f76
Ray Luna <leon.luna.ray@gmail.com>
2024-12-26 21:13:40 -0800

    update

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # NEW
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4173"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
